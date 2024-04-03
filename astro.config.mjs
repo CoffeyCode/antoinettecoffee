07d2d784b7b6977a0409c923284f3d9515e81ef3
Denartha10 <mahonniall2012@gmail.com>
2024-04-03 18:48:20 +0100

    Adding storyblok support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,26 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://yoursite.com/",
-  integrations: [tailwind(), sitemap()]
+  integrations: [
+    tailwind(),
+    sitemap(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        intro: "storyblok/Intro",
+      },
+      apiOptions: {
+        // Choose your Storyblok space region
+        region: "eu", // optional,  or 'eu' (default)
+      },
+    }),
+  ],
 });
+
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-interface ImportMetaEnv {
-  readonly BUILDER_API_PUBLIC_KEY: string;
-}
