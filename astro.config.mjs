103bf887d7f04d562bc40036b72af8765eb72a25
Denartha10 <mahonniall2012@gmail.com>
2024-04-15 20:23:32 +0100

    added confetti need to add stripe link

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import sitemap from "@astrojs/sitemap";
 import storyblok from "@storyblok/astro";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import { loadEnv } from "vite";
+import db from "@astrojs/db";
 const env = loadEnv("", process.cwd(), "STORYBLOK");
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,34 +14,30 @@ export default defineConfig({
   vite: {
     plugins: [basicSsl()],
     server: {
-      https: true,
-    },
+      https: true
+    }
   },
-  integrations: [
-    tailwind(),
-    sitemap(),
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        content: "storyblok/Content",
-        cta: "storyblok/Cta",
-        faq: "storyblok/Faq",
-        footer: "storyblok/Footer",
-        "heading-block": "storyblok/HeadingBlock",
-        intro: "storyblok/Intro",
-        reward: "storyblok/Reward",
-        signUpForm: "storyblok/SignUpForm",
-        "message-board": "storyblok/Message-Board",
-        thankYou: "storyblok/ThankYou",
-        yourIn: "storyblok/YourIn",
-        pricing: "storyblok/Pricing",
-        media: "storyblok/Media",
-        link: "storyblok/Link",
-      },
-      apiOptions: {
-        // Choose your Storyblok space region
-        region: "eu", // optional,  or 'eu' (default)
-      },
-    }),
-  ],
+  integrations: [tailwind(), sitemap(), storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      content: "storyblok/Content",
+      cta: "storyblok/Cta",
+      faq: "storyblok/Faq",
+      footer: "storyblok/Footer",
+      "heading-block": "storyblok/HeadingBlock",
+      intro: "storyblok/Intro",
+      reward: "storyblok/Reward",
+      signUpForm: "storyblok/SignUpForm",
+      "message-board": "storyblok/Message-Board",
+      thankYou: "storyblok/ThankYou",
+      yourIn: "storyblok/YourIn",
+      pricing: "storyblok/Pricing",
+      media: "storyblok/Media",
+      link: "storyblok/Link"
+    },
+    apiOptions: {
+      // Choose your Storyblok space region
+      region: "eu" // optional,  or 'eu' (default)
+    }
+  }), db()]
 });
